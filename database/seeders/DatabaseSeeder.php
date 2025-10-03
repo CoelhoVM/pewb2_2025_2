0bb8d44ef427616e6938624ef0278b7d7c6058bc
CoelhoVM <vitorya.m@aluno.ifsc.edu.br>
2025-10-03 15:17:40 -0300

    Add Curso, Matricula, and Turma models, controllers, factories, migrations, and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,9 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategoriaAlunoSeeder::class,
             AlunoSeeder::class,
+            CursoSeeder::class,
+            TurmaSeeder::class,
+            MatriculaSeeder::class,
         ]);
     }
 }
